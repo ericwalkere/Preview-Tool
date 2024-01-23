719458a3b8c83b67d9e064428b4a19e09e88faa4
ericwalkerw <an.pham@enotion.io>
2024-01-23 14:39:23 +0700

    load event by anim

--- assets/scripts/component/LoadData.js
+++ assets/scripts/component/LoadData.js
@@ -1,13 +1,3 @@
-// Learn cc.Class:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/class.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/class.html
-// Learn Attribute:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/reference/attributes.html
-//  - [English] http://docs.cocos2d-x.org/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - [Chinese] https://docs.cocos.com/creator/manual/zh/scripting/life-cycle-callbacks.html
-//  - [English] https://www.cocos2d-x.org/docs/creator/manual/en/scripting/life-cycle-callbacks.html
-
 const Emitter = require("EventEmitter");
 const EventCode = require("EventCode");
 cc.Class({
@@ -31,11 +21,16 @@ cc.Class({
         this.type = type;
     },
 
+    onDestroy(){
+        cc.log('alo');
+    },
+
     onClick() {
         switch (this.type) {
             case "anim":
+                Emitter.instance.emit(EventCode.SPINE_POOL.REMOVE_EVENT_CHILDREN);
+                const set = new Set();
                 Emitter.instance.emit(EventCode.TIMELINE.SET_CHILDREN);
-
                 Emitter.instance.emit(
                     EventCode.SPINE_CTRL.SET_ANIM,
                     this.textValue
@@ -49,14 +44,19 @@ cc.Class({
                             anim.events[i].name
                         );
                     }
+                    this.json.animations[this.textValue].events.forEach((e) => {
+                        set.add(e.name);
+                    });
                 }
+                set.forEach((element) => {
+                    Emitter.instance.emit(EventCode.SPINE_POOL.LOAD_EVENT_BY_ANIM, element);
+                });
                 break;
             case "skin":
                 cc.log("click skin :", this.textValue);
                 break;
             case "event":
                 cc.log("click event :", this.textValue);
-                // Emitter.instance.emit('hi', this.textValue);
                 break;
         }
     },
