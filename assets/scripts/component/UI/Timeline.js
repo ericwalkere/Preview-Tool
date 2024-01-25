f66be1ec3601126df24e36e6c63a234e5822a1a8
Hieu Cao <hieu.cao@enotion.io>
2024-01-25 14:01:44 +0700

    Update loop spine controller

--- assets/scripts/component/UI/Timeline.js
+++ assets/scripts/component/UI/Timeline.js
@@ -15,16 +15,8 @@ cc.Class({
     },
 
     onLoad() {
-        registerEvent(
-            EventCode.TIMELINE.SET_EVENT_KEY,
-            this.createEventKey,
-            this
-        );
-        registerEvent(
-            EventCode.TIMELINE.SET_CHILDREN,
-            this.removeChildren,
-            this
-        );
+        registerEvent(EventCode.TIMELINE.SET_EVENT_KEY, this.createEventKey, this);
+        registerEvent(EventCode.TIMELINE.SET_CHILDREN, this.removeChildren, this);
     },
 
     setDurationTime(durationTime) {
@@ -32,27 +24,27 @@ cc.Class({
     },
 
     updateTimeline(currentTime) {
-        const progress =
-            this._durationTime === 0 ? 1 : currentTime / this._durationTime;
+        const progress = this._durationTime === 0 ? 1 : currentTime / this._durationTime;
         this.slider.progress = progress;
     },
 
     onSlide(slide) {
         const currentTime = slide.progress * this._durationTime;
         Emitter.instance.emit(EventCode.SPINE_CTRL.UPDATE_TIME, currentTime);
+        Emitter.instance.emit(EventCode.SPINE_CTRL.SET_PAUSED, true);
     },
 
     createEventKey(eventTime, eventName) {
         const percent = eventTime / this._durationTime;
         const key = cc.instantiate(this.eventKey);
-        key.getComponent('clickEvent').hint(eventName);
+        key.getComponent("clickEvent").hint(eventName);
         key.x = percent * 600;
         key.parent = this.eventNode;
     },
 
     removeChildren() {
         const arr = this.eventNode.children;
-        arr.forEach(element => {
+        arr.forEach((element) => {
             element.destroy();
         });
     },
